5fde0cb5475dfef98920adf1f63886a32819fb4a
raaynaldo <raaynaldo@hotmail.com>
2020-11-05 10:18:37 -0500

    create appeareances controller and view done.

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,4 +1,20 @@
 class AppearancesController < ApplicationController
   def new
+    @appearances = Appearance.new
+  end
+
+  def create
+    @appearances = Appearance.new(appearances_params)
+
+    if @appearances.save
+      redirect_to episode_path(@appearances.episode_id)
+    else
+      return :new
+    end
+  end
+
+  private
+  def appearances_params
+    params.require(:appearance).permit(:episode_id, :guest_id, :rating)
   end
 end
