74b991877492a22c0dd19fcfc29eb820953461b0
raaynaldo <raaynaldo@hotmail.com>
2020-11-05 10:21:05 -0500

    add to_s method to episode and update the view.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,8 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guest, through: :appearances
+
+    def to_s
+        "Episode #{self.number} - #{self.date.strftime("%B %d, %Y")}"
+    end
 end
