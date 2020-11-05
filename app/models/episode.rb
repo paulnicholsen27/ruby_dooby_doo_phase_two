c2cf2516600a90819bbcc1bcccfe85298f25b153
raaynaldo <raaynaldo@hotmail.com>
2020-11-05 10:29:05 -0500

    update has_many guests missing pluralize.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,8 +1,12 @@
 class Episode < ApplicationRecord
     has_many :appearances
-    has_many :guest, through: :appearances
+    has_many :guests, through: :appearances
 
     def to_s
-        "Episode #{self.number} - #{self.date.strftime("%B %d, %Y")}"
+        "Episode #{self.number} - #{self.date_to_s}"
+    end
+
+    def date_to_s
+        self.date.strftime("%B %d, %Y")
     end
 end
