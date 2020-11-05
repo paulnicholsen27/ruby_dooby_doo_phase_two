a3ed30af36e32464d3975337642d0ffc49091b48
jrcliff <justoon7.jc@gmail.com>
2020-11-05 12:54:58 -0500

    Justin Clifford

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,24 @@
+class AppearancesController < ApplicationController
+    def index
+        @appearances = Appearance.all
+    end
+
+    def new
+        @appearance = Appearance.new
+    end
+
+    def create
+        @appearance = Appearance.new(appearance_params)
+        if @appearance.save
+            redirect_to episode_path(@appearance.episode)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def appearance_params
+        params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    end
+end
