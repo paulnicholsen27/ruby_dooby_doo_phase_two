87879e5eda0d61a0b4bf3d7e512e3c217487b3ee
raaynaldo <raaynaldo@hotmail.com>
2020-11-05 11:58:25 -0500

    update ordering page

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -7,11 +7,4 @@ class Guest < ApplicationRecord
         avg = (ratings.inject(0.0) { |sum, el| sum + el } / ratings.size).round(1)
         avg = avg.nan? ? 0 : avg
     end
-
-    def self.order_by_rating
-        Guest.select('guests.id, guests.name, avg(appearances.rating)')
-       .left_outer_joins(:appearances)
-       .group('guests.id')
-       .order('avg(appearances.rating) desc')
-    end
 end
